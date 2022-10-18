22d895e8f4c530a9e2e65452c10c0b35be67fb20
diogo Menezes <diomenezes@hotmail.com>
2022-10-17 20:59:43 -0400

    Add files via upload

---
+++ autoclean Ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+sudo apt-get autoclean -y && sudo apt-get clean -y
+sudo apt-get autoremove -y && sudo apt --fix-broken install -y
+#sudo apt-get update -y && sudo apt-get upgrade -y
+sudo journalctl --vacuum-time=1s
+sudo snap set system refresh.retain=2
+
+set -eu
+
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+rm -rf ~/.cache/thumbnails/*
