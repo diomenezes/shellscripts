d413ea647cf26c21d65f99f062a97b7dcab6b094
dMenezes <diomenezes@hotmail.com>
2023-07-29 13:50:20 -0300

    Update After_Install_Fedora38.sh

--- After_Install_Fedora38.sh
+++ After_Install_Fedora38.sh
@@ -35,6 +35,7 @@ sudo dnf install -y  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 2>> $LOG/Erro.log
 sudo dnf update -y 2>> $LOG/Erro.log
 sudo dnf upgrade -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "################################################"
@@ -52,6 +53,8 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install catfish -y  2>> $LOG/Erro.log
+sleep 3
+
 
 clear
 echo "##########################################"
@@ -61,6 +64,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install gnome-tweaks -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -70,6 +74,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install htop -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -79,6 +84,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install testdisk -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -88,6 +94,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo echo  vm.swappiness = 10 >> /etc/sysctl.conf 2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -97,6 +104,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install cups-pdf -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -106,6 +114,7 @@ echo ""
 echo "##########################################"
 sleep 3
 flatpak install flathub org.videolan.VLC -y 2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -114,7 +123,78 @@ echo " Intalando o Angry IP"
 echo ""
 echo "##########################################"
 sleep 3
-https://github.com/angryip/ipscan/releases/download/3.9.1/ipscan-3.9.1-1.x86_64.rpm -y 2>> $LOG/Erro.log
+sudo dnf install https://github.com/angryip/ipscan/releases/download/3.9.1/ipscan-3.9.1-1.x86_64.rpm -y 2>> $LOG/Erro.log
+sleep 3
+
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o Binance"
+echo ""
+echo "##########################################"
+sleep 3
+sudo dnf install https://download.binance.com/electron-desktop/linux/production/binance-x86_64-linux.rpm -y 2>> $LOG/Erro.log
+sleep 3
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o mullvad"
+echo ""
+echo "##########################################"
+sleep 3
+sudo dnf install https://mullvad.net/en/download/app/rpm/latest -y 2>> $LOG/Erro.log
+sleep 3
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o Hidamari"
+echo ""
+echo "##########################################"
+sleep 3
+flatpak install flathub io.github.jeffshee.Hidamari -y 2>> $LOG/Erro.log
+sleep 3
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o Skype"
+echo ""
+echo "##########################################"
+sleep 3
+flatpak install flathub com.skype.Client -y 2>> $LOG/Erro.log
+sleep 3 
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o telegram"
+echo ""
+echo "##########################################"
+sleep 3 
+flatpak install flathub org.telegram.desktop -y 2>> $LOG/Erro.log
+sleep 3 
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o WhatsAppDesktop"
+echo ""
+echo "##########################################"
+sleep 3 
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y 2>> $LOG/Erro.log
+sleep 3 
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o tokodon"
+echo ""
+echo "##########################################"
+sleep 3 
+flatpak install flathub org.kde.tokodon -y 2>> $LOG/Erro.log
 
 clear
 echo "##########################################"
@@ -124,6 +204,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install neofetch -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -133,6 +214,17 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install gcc -y  2>> $LOG/Erro.log
+sleep 3
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o yubico"
+echo ""
+echo "##########################################"
+sleep 3
+flatpak install flathub com.yubico.yubioath -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -143,6 +235,7 @@ echo "##########################################"
 sleep 3
 sudo dnf install cabextract lzip p7zip p7zip-plugins unrar -y  2>> $LOG/Erro.log
 sudo dnf install pigz pbzip2 pxz -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -152,6 +245,27 @@ echo ""
 echo "##########################################"
 sleep 3
 flatpak install flathub org.qbittorrent.qBittorrent -y  2>> $LOG/Erro.log
+sleep 3
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o Zenmap"
+echo ""
+echo "##########################################"
+sleep 3
+flatpak install flathub org.nmap.Zenmap -y  2>> $LOG/Erro.log
+sleep 3
+
+clear
+echo "##########################################"
+echo ""
+echo " Intalando o trimmer"
+echo ""
+echo "##########################################"
+sleep 3f
+latpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -161,6 +275,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install nvme-cli -y 2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -170,7 +285,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install stacer -y 2>> $LOG/Erro.log
-
+sleep 3
 
 clear
 echo "##########################################"
@@ -180,6 +295,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install smartmontools -y 2>> $LOG/Erro.log
+sleep 3
 
 echo "#####################################################"
 echo ""
@@ -187,7 +303,7 @@ echo " ATENCAO A LINHA ABAIXO SERA ADD no /etc/hosts.denny "
 echo " 	 ALL:ALL  	"
 echo ""
 echo "######################################################"
-sleep 3
+sleep 5
 sudo echo " ALL:ALL" >> /etc/hosts.deny 2>> $LOG/Erro.log
 						
 clear
@@ -197,7 +313,8 @@ echo " Intalando o nmap"
 echo ""
 echo "##########################################"
 sleep 3
-dnf install nmap -y 2>> $LOG/Erro.log
+sudo dnf install nmap -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -206,7 +323,8 @@ echo " Intalando o calibre"
 echo ""
 echo "##########################################"
 sleep 3
-flatpak install flathub com.calibre_ebook.calibre -y
+flatpak install flathub com.calibre_ebook.calibre -y 2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -216,6 +334,7 @@ echo ""
 echo "##########################################"
 sleep 3
 flatpak install flathub com.vixalien.sticky -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -225,6 +344,7 @@ echo ""
 echo "##########################################"
 sleep 3
 flatpak install flathub com.discordapp.Discord -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -234,6 +354,7 @@ echo ""
 echo "##########################################"
 sleep 3
 sudo dnf install VirtualBox -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -243,6 +364,7 @@ echo ""
 echo "##########################################"
 sleep 3			
 sudo dnf install dnfdragora-gui -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -252,6 +374,7 @@ echo ""
 echo "##########################################"
 sleep 3	
 sudo dnf install https://cdn.insynchq.com/builds/linux/insync-3.8.6.50504-fc38.x86_64.rpm -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -261,6 +384,7 @@ echo ""
 echo "##########################################"
 sleep 3	
 sudo dnf install google-chrome-stable -y 2>> $LOG/Erro.log
+sleep 3
 			
 clear
 echo "##########################################"
@@ -270,7 +394,7 @@ echo ""
 echo "##########################################"
 sleep 3	
 flatpak install flathub com.valvesoftware.Steam -y 2>> $LOG/Erro.log			
-			
+sleep 3
 			
 clear
 echo "##########################################"
@@ -278,6 +402,7 @@ echo ""
 echo " Instalacao fedora-workstation-repositories"
 echo ""
 echo "##########################################"
+sleep 3
 sudo dnf install fedora-workstation-repositories -y 2>> $LOG/Erro.log
 sleep 3
 
@@ -287,14 +412,18 @@ echo ""
 echo " Instalacao  nvidia-driver"
 echo ""
 echo "##########################################"
+sleep 3
 sudo dnf config-manager rpmfusion-nonfree-nvidia-driver --set-enabled 2>> $LOG/Erro.log
 sleep 3
+
 clear
 sudo dnf install akmod-nvidia acpi xorg-x11-drv-nvidia-cuda vulkan -y 2>> $LOG/Erro.log 
 sleep 3
+
 clear
 sudo dnf install mesa-dri-drivers.i686 mesa-libGL.i686 xorg-x11-drv-intel -y 2>> $LOG/Erro.log
 sleep 3
+
 clear
 sudo dnf install xorg-x11-drv-nvidia-libs.i686 -y 2>> $LOG/Erro.log			
 sleep 3
@@ -305,6 +434,7 @@ echo ""
 echo " Instalacao r-studio"
 echo ""
 echo "##########################################"
+sleep 3
 sudo dnf install https://www.r-studio.com/downloads/RLinux5_x64.rpm -y 2>> $LOG/Erro.log
 sleep 3
 
@@ -314,7 +444,8 @@ echo ""
 echo " Instalacao TeamView"
 echo ""
 echo "##########################################"
-dnf install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -y 2>> $LOG/Erro.log
+sleep 3
+sudo dnf install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -y 2>> $LOG/Erro.log
 sleep 3
 
 clear
@@ -323,6 +454,7 @@ echo ""
 echo " Instalacao OnlyOffice"
 echo ""
 echo "##########################################"
+sleep 3
 flatpak install flathub org.onlyoffice.desktopeditors -y 2>> $LOG/Erro.log
 sleep 3
 
@@ -332,16 +464,19 @@ echo ""
 echo " Instalacao ZOOM"
 echo ""
 echo "##########################################"
+sleep 3
 flatpak install flathub us.zoom.Zoom -y 2>> $LOG/Erro.log
+sleep 3
 			
-
 clear
 echo "##########################################"
 echo ""
 echo " Instalacao TEAMS"
 echo ""
 echo "##########################################"
-flatpak install flathub com.github.IsmaelMartinez.teams_for_linux
+sleep 3
+flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y 2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
@@ -349,7 +484,9 @@ echo ""
 echo " Instalacao Lutris"
 echo ""
 echo "##########################################"
-flatpak install flathub net.lutris.Lutris
+sleep 3
+flatpak install flathub net.lutris.Lutris -y 2>> $LOG/Erro.log
+sleep 3
 
 
 clear
@@ -358,14 +495,17 @@ echo ""
 echo " Instalacao Evolution"
 echo " "
 echo "##########################################"
-flatpak install flathub org.gnome.Evolution
+sleep 3
+flatpak install flathub org.gnome.Evolution -y 2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo "##########################################"
 echo " Instalacao de Remmia: " 
 echo "##########################################"
 echo " "
-flatpak install flathub org.remmina.Remmina
+sleep 3
+flatpak install flathub org.remmina.Remmina -y 2>> $LOG/Erro.log
 sleep 3
 
 clear
@@ -373,7 +513,8 @@ echo "##########################################"
 echo " Instalacao de Flameshot: " 
 echo "##########################################"
 echo ""
-flatpak install flathub org.flameshot.Flameshot
+sleep 3
+flatpak install flathub org.flameshot.Flameshot -y 2>> $LOG/Erro.log
 sleep 3
 
 clear
@@ -381,7 +522,8 @@ echo "##########################################"
 echo " Instalacao de Wireshark: " 
 echo "##########################################"
 echo ""
-flatpak install flathub org.wireshark.Wireshark
+sleep 3
+flatpak install flathub org.wireshark.Wireshark -y 2>> $LOG/Erro.log
 sleep 3
 
 clear
@@ -389,7 +531,8 @@ echo "##########################################"
 echo " Instalacao de gnome-extensions-app " 
 echo "##########################################"
 echo ""
-sudo dnf install gnome-extensions-app -y 2>> $LOG/Erro.log
+sleep 3
+sudo dnf install gnome-extensions-app -y 2>> $LOG/Erro.log -y
 sleep 3
 
 clear
@@ -397,16 +540,17 @@ echo "##########################################"
 echo " Instalacao de extension-dash-to-doc " 
 echo "##########################################"
 echo ""
-sudo dnf install gnome-shell-extension-dash-to-dock -y 2>> $LOG/Erro.log
 sleep 3
-
+sudo dnf install gnome-shell-extension-dash-to-dock -y 2>> $LOG/Erro.log -y
+sleep 3
 
 clear
 echo "##########################################"
 echo " Instalacao de extension-appindicator " 
 echo "##########################################"
 echo ""
-sudo dnf install gnome-shell-extension-appindicator -y 2>> $LOG/Erro.log
+sleep 3
+sudo dnf install gnome-shell-extension-appindicator -y 2>> $LOG/Erro.log -y
 sleep 3
 
 clear
@@ -414,15 +558,16 @@ echo "##########################################"
 echo " Instalacao do system-monitoring-center " 
 echo "##########################################"
 echo ""
-flatpak install flathub io.github.hakandundar34coding.system-monitoring-center
+flatpak install flathub io.github.hakandundar34coding.system-monitoring-center -y 2>> $LOG/Erro.log
 
 clear
 echo "##########################################"
 echo " Instalacao de Zmap " 
 echo "##########################################"
 echo ""
-sudo dnf install zmap -y 
-
+sleep 3
+sudo dnf install zmap -y  2>> $LOG/Erro.log
+sleep 3
 
 clear
 echo " Instalacao MANUAL das Extensions:" 
@@ -443,9 +588,9 @@ read CONTINUAR
 clear
 echo "Fim da da Instalacao"
 sleep 15
-chown $USUARIO.$USUARIO $LOG/Erro.log
-chmod 755 $LOG/Erro.log
-cat $LOG/Erro.log | less
+sudo chown $USUARIO.$USUARIO $LOG/Erro.log
+sudo chmod 755 $LOG/Erro.log
+sudo cat $LOG/Erro.log | less
 
 echo ""
 echo "##########################################"
